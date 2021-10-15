a75bd2480f130462efb3db00f4f16fd3e0fd79fa
JoseLiraa <josmliraa@gmail.com>
2021-10-14 21:41:20 -1100

    Agregando pantalla carrito

--- src/screens/Carrito.js
+++ src/screens/Carrito.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-
-export default function Carrito(){
-    return(
-        <View>
-            <Text>Estamos en el Carrito</Text>
-        </View>
-    );
+import React from "react";
+import { View, Text } from "react-native";
+import Encabezado from "../../components/Encabezado";
+import Pie from "../../components/Pie";
+export default function Carrito() {
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Encabezado />
+        <Pie />
+      </View>
+      <Button
+        title="Ir a Carrito"
+        onPress={() => navigation.navigate("carrito")}
+      ></Button>
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+  },
+});
